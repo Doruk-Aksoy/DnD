9cdcc9f62f87abc0e41aecd3c5e58d8488c2a06d
Doruk Aksoy <dorukaksoy.metu@gmail.com>
2018-05-13 00:31:45 +0300

    Fixed failure of acs compilation if directories didn't exist

--- acs/compile_acs.bat
+++ acs/compile_acs.bat
@@ -1,5 +1,8 @@
 REM for %%F in (*.bcs) do %bcc% "%%~nF.bcs" "..\pk3\acs\%%~nF.lmp"
 
+test -d "..\dnd\acs\" || mkdir "..\dnd\acs\"
+test -d "..\monsters\acs\" || mkdir "..\monsters\acs\"
+
 %bcc% "dnd.bcs" "..\dnd\acs\dnd.lmp"
 %bcc% "dnd_database.bcs" "..\dnd\acs\dnd_db.lmp"
 %bcc% "dnd_menu.bcs" "..\dnd\acs\dnd_menu.lmp"
