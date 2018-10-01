c84abcb3079a471e7503f46f267a4828d4b1a976
Doruk Aksoy <dorukaksoy.metu@gmail.com>
2018-10-01 17:09:06 +0300

    Fixed menu highlight bug on buttons

--- acs/DnD_MenuFuncs.h
+++ acs/DnD_MenuFuncs.h
@@ -1027,7 +1027,7 @@ void ProcessTrade (int pnum, int posy, int low, int high, int tradeflag, bool gi
 void DrawHighLightBar (int posy, bool drawlit) {
 	if(posy == MAINBOX_NONE)
 		HudMessage(s:""; HUDMSG_PLAIN, RPGMENUHIGHLIGHTID, -1, 47.1, 99.1, 0.0, 0.0);
-	else {
+	else if(posy < FIRST_CLICKABLE_BOXID) {
 		--posy;
 		int yadd = 0;
 		if(posy > 0)
