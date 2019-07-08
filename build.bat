3fdc240a852f3fa696f6aa6666caf1d21564a4b5
deguix <deguix@gmail.com>
2019-07-08 02:46:53 -0400

    Revert "Fixed misnamed monster frame files on Windows. Rename to proper files will now happen on build."
    This reverts commit bbdc9287206c16a4743af382c86c2249b8c5ce3e.

--- build.bat
+++ build.bat
@@ -20,14 +20,6 @@ echo Packing all files into pk7's...
 FOR /F "tokens=*" %%g in ('git describe --tags') do (SET version=%%g)
 7z u -t7z dnd%version%.pk7 -uq0 .\dnd\* -mx=9 >nul
 7z u -t7z dnd_monsters%version%.pk7 -uq0 .\monsters\* -mx=9 >nul
-
-rem Renaming files that have invalid chars on Windows:
-7z d dnd_monsters%version%.pk7 .\Sprites\Monsters\ArchVile\WILE[1.png .\Sprites\Monsters\ArchVile\WILE[2.png .\Sprites\Monsters\ArchVile\WILE[3.png .\Sprites\Monsters\ArchVile\WILE[4.png .\Sprites\Monsters\ArchVile\WILE[5.png .\Sprites\Monsters\ArchVile\WILE[6.png .\Sprites\Monsters\ArchVile\WILE[7.png .\Sprites\Monsters\ArchVile\WILE[8.png >/dev/null
-7z rn dnd_monsters%version%.pk7 .\Sprites\Monsters\ArchVile\1.png .\Sprites\Monsters\ArchVile\WILE[1.png .\Sprites\Monsters\ArchVile\2.png .\Sprites\Monsters\ArchVile\WILE[2.png .\Sprites\Monsters\ArchVile\3.png .\Sprites\Monsters\ArchVile\WILE[3.png .\Sprites\Monsters\ArchVile\4.png .\Sprites\Monsters\ArchVile\WILE[4.png .\Sprites\Monsters\ArchVile\5.png .\Sprites\Monsters\ArchVile\WILE[5.png .\Sprites\Monsters\ArchVile\6.png .\Sprites\Monsters\ArchVile\WILE[6.png .\Sprites\Monsters\ArchVile\7.png .\Sprites\Monsters\ArchVile\WILE[7.png .\Sprites\Monsters\ArchVile\8.png .\Sprites\Monsters\ArchVile\WILE[8.png >/dev/null
-
-7z d dnd_monsters%version%.pk7 .\Sprites\Monsters\ArchVile\WILE]1.png .\Sprites\Monsters\ArchVile\WILE]2.png .\Sprites\Monsters\ArchVile\WILE]3.png .\Sprites\Monsters\ArchVile\WILE]4.png .\Sprites\Monsters\ArchVile\WILE]5.png .\Sprites\Monsters\ArchVile\WILE]6.png .\Sprites\Monsters\ArchVile\WILE]7.png .\Sprites\Monsters\ArchVile\WILE]8.png >/dev/null
-7z rn dnd_monsters%version%.pk7 .\Sprites\Monsters\ArchVile\1.png .\Sprites\Monsters\ArchVile\WILE]1.png .\Sprites\Monsters\ArchVile\2.png .\Sprites\Monsters\ArchVile\WILE]2.png .\Sprites\Monsters\ArchVile\3.png .\Sprites\Monsters\ArchVile\WILE]3.png .\Sprites\Monsters\ArchVile\4.png .\Sprites\Monsters\ArchVile\WILE]4.png .\Sprites\Monsters\ArchVile\5.png .\Sprites\Monsters\ArchVile\WILE]5.png .\Sprites\Monsters\ArchVile\6.png .\Sprites\Monsters\ArchVile\WILE]6.png .\Sprites\Monsters\ArchVile\7.png .\Sprites\Monsters\ArchVile\WILE]7.png .\Sprites\Monsters\ArchVile\8.png .\Sprites\Monsters\ArchVile\WILE]8.png >/dev/null
-
 7z u -t7z dnd_onlyammo%version%.pk7 -uq0 .\onlyammo\* -mx=9 >nul
 
 echo Done.
--- test.bat
+++ test.bat
@@ -19,16 +19,7 @@ echo Packing all files into pk7's...
 
 7z u -t7z dnd.pk7 -uq0 .\dnd\* -mx=9 -ms=off >nul
 7z u -t7z dnd_monsters.pk7 -uq0 .\monsters\* -mx=9 -ms=off >nul
-
-rem Renaming files that have invalid chars on Windows:
-7z d dnd_monsters.pk7 .\Sprites\Monsters\ArchVile\WILE[1.png .\Sprites\Monsters\ArchVile\WILE[2.png .\Sprites\Monsters\ArchVile\WILE[3.png .\Sprites\Monsters\ArchVile\WILE[4.png .\Sprites\Monsters\ArchVile\WILE[5.png .\Sprites\Monsters\ArchVile\WILE[6.png .\Sprites\Monsters\ArchVile\WILE[7.png .\Sprites\Monsters\ArchVile\WILE[8.png >/dev/null
-7z rn dnd_monsters.pk7 .\Sprites\Monsters\ArchVile\1.png .\Sprites\Monsters\ArchVile\WILE[1.png .\Sprites\Monsters\ArchVile\2.png .\Sprites\Monsters\ArchVile\WILE[2.png .\Sprites\Monsters\ArchVile\3.png .\Sprites\Monsters\ArchVile\WILE[3.png .\Sprites\Monsters\ArchVile\4.png .\Sprites\Monsters\ArchVile\WILE[4.png .\Sprites\Monsters\ArchVile\5.png .\Sprites\Monsters\ArchVile\WILE[5.png .\Sprites\Monsters\ArchVile\6.png .\Sprites\Monsters\ArchVile\WILE[6.png .\Sprites\Monsters\ArchVile\7.png .\Sprites\Monsters\ArchVile\WILE[7.png .\Sprites\Monsters\ArchVile\8.png .\Sprites\Monsters\ArchVile\WILE[8.png >/dev/null
-
-7z d dnd_monsters.pk7 .\Sprites\Monsters\ArchVile\WILE]1.png .\Sprites\Monsters\ArchVile\WILE]2.png .\Sprites\Monsters\ArchVile\WILE]3.png .\Sprites\Monsters\ArchVile\WILE]4.png .\Sprites\Monsters\ArchVile\WILE]5.png .\Sprites\Monsters\ArchVile\WILE]6.png .\Sprites\Monsters\ArchVile\WILE]7.png .\Sprites\Monsters\ArchVile\WILE]8.png >/dev/null
-7z rn dnd_monsters.pk7 .\Sprites\Monsters\ArchVile\1.png .\Sprites\Monsters\ArchVile\WILE]1.png .\Sprites\Monsters\ArchVile\2.png .\Sprites\Monsters\ArchVile\WILE]2.png .\Sprites\Monsters\ArchVile\3.png .\Sprites\Monsters\ArchVile\WILE]3.png .\Sprites\Monsters\ArchVile\4.png .\Sprites\Monsters\ArchVile\WILE]4.png .\Sprites\Monsters\ArchVile\5.png .\Sprites\Monsters\ArchVile\WILE]5.png .\Sprites\Monsters\ArchVile\6.png .\Sprites\Monsters\ArchVile\WILE]6.png .\Sprites\Monsters\ArchVile\7.png .\Sprites\Monsters\ArchVile\WILE]7.png .\Sprites\Monsters\ArchVile\8.png .\Sprites\Monsters\ArchVile\WILE]8.png >/dev/null
-
 7z u -t7z dnd_onlyammo.pk7 -uq0 .\onlyammo\* -mx=9 -ms=off >nul
 
 echo Done.
 @echo on
-
