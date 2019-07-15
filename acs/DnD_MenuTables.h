c6e869bccfb2b2e3a79a3b2ec67bcbadcd46fb50
Doruk Aksoy <dorukaksoy.metu@gmail.com>
2019-07-15 22:31:59 +0300

    Vindicator fix attempt #2. Fixed Ravager armor not properly repairing itself.

--- acs/DnD_MenuTables.h
+++ acs/DnD_MenuTables.h
@@ -1376,7 +1376,7 @@ str WeaponExplanation[MAXSHOPWEAPONS] = {
 	
 	"The newest BFG model 32768 devastates with 600 - 900 damage on impact and 384 damage in a 160 unit radius. Shoots 64 tracers.",
 	"Devastator launches five mini rockets each doing 64 to 80 with 32 radius damage in 96 units.",
-	"Fires a destructive orb doing 425 damage on impact and 384 damage in a 768 unit radius. Creates 6 smaller explosions doing 200 damage in a 256 unit radius.",
+	"Fires a destructive orb doing 425 damage on impact and 448 damage in a 768 unit radius. Creates 6 smaller explosions doing 200 damage in a 256 unit radius.",
 	"Fires ionized energy doing 125 impact and 160 area damage in 160 unit radius, then 80 in 80 and 53 in 60.",
 	"Launches a ball of lightning that zaps 5 nearest enemies for 115 damage in 420 units. On impact deals 250-500 and 250 radius damage in 96 units. Altfire zaps all enemies in a large radius.",
 	"Gauss Rifle fires a magnetic pulse dealing 100 on hit and 192 radius damage in a 96 unit radius. Alt fire zooms, amplifying the damage for each zoom.",
