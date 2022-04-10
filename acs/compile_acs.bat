e1b79687bd4912d197886cc7a78072cf2883c614
Doruk-Aksoy <dorukaksoy.metu@gmail.com>
2022-04-10 14:57:37 +0300

    Fixed gitignore destroying .lmp files, and restored thewm

--- acs/compile_acs.bat
+++ acs/compile_acs.bat
@@ -7,8 +7,8 @@ if not exist "..\monsters\acs" mkdir "..\monsters\acs"
 echo Compiling acs code... If errors pop up, they will show up below:
 echo ----------------------------------------------------------------
 echo.
-%bcs% "dnd.bcs" "..\dnd\acs\dnd.lmp"
-%bcs% "dnd_database.bcs" "..\dnd\acs\dnd_db.lmp"
-%bcs% "dnd_menu.bcs" "..\dnd\acs\dnd_menu.lmp"
-%bcs% "dnd_mons.bcs" "..\monsters\acs\dnd_mons.lmp"
+%bcs% "dnd.bcs" "..\dnd\acs\dnd.o"
+%bcs% "dnd_database.bcs" "..\dnd\acs\dnd_db.o"
+%bcs% "dnd_menu.bcs" "..\dnd\acs\dnd_menu.o"
+%bcs% "dnd_mons.bcs" "..\monsters\acs\dnd_mons.o"
 echo ----------------------------------------------------------------
